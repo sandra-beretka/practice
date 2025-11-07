2ff3aaefbd00b763dceb3af7008fb20b0ed49738
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-07 14:38:55 +0100

    Added LogicBuilding excercise: Checking if a number is even or odd.

---
+++ Data Structures/LogicBuilding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LogicBuilding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please give me a number: ");
+            string input = Console.ReadLine();
+
+            int number = 0;
+            if(!int.TryParse(input, out number))
+            {
+                Console.WriteLine("The input was not in correct format.");
+                return;
+            }
+
+            CheckEvenOrOdd(number);
+        }
+
+        private static void CheckEvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
