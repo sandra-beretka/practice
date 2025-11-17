107bd54cb0408abbdece9afd7f368b2a7ecb756b
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-17 17:08:42 +0100

    Added Prime Numbers

---
+++ Data Structures/LogicBuilding.UnitTests/PrimeNumbersTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class PrimeNumbersTests
+    {
+
+        [TestMethod]
+        [DataRow(0, false)]
+        [DataRow(-1, false)]
+        [DataRow(2, true)]
+        [DataRow(3, true)]
+        [DataRow(4, false)]
+        [DataRow(5, true)]
+        [DataRow(6, false)]
+        [DataRow(7, true)]
+        [DataRow(8, false)]
+        [DataRow(9, false)]
+
+        public void PrimeNumbers_ByParameters_ReturnsTheExpectedValue(int n, bool expected)
+        {
+            // Act
+            bool actual = PrimeNumbers.IsPrime(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ Data Structures/LogicBuilding/PrimeNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class PrimeNumbers
+    {
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+                        
+                for (int i = 2; i < n; i++)
+                    if (n % i == 0)
+                        return false;
+
+                return true;            
+        }
+    }
+}
