9ac93e11865ed1587eb2910124d325408eb3fb26
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-17 17:20:23 +0100

    Added Power of Numbers

---
+++ Data Structures/LogicBuilding.UnitTests/PowerofNumbersTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class PowerofNumbersTests
+    {
+        [TestMethod]
+        [DataRow(1,1, true)]
+        [DataRow(10, 1, true)]
+        [DataRow(10, 1000, true)]
+        [DataRow(10, 1001, false)]
+
+        public void PowerofNumbers_ByParameters_ReturnsTheExpectedValue(int x, int y, bool expected)
+        {
+            // Act
+            bool actual = PowerofNumers.IsPower(x,y);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/PowerofNumers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class PowerofNumers
+    {
+        public static bool IsPower(int x, int y)
+        {
+            if (x == 1)
+                return (y == 1);
+
+            
+            int power = 1;
+            while (power < y)
+                power *= x;
+
+            
+            return (power == y);
+        }
+    }
+}
