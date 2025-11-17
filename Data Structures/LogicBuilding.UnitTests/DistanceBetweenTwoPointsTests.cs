4042efb8512649622fec6999b2f532a86dd29020
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-17 17:31:14 +0100

    Added Distance Between Two Points

---
+++ Data Structures/LogicBuilding.UnitTests/DistanceBetweenTwoPointsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class DistanceBetweenTwoPointsTests
+    {
+        [TestMethod]
+        [DataRow(3,4,7,7,5)]
+        [DataRow(3,4,4,3, 1.4142135623730951)]
+
+        public void DistanceBetweenTwoPoints_ByParameters_ReturnsTheExpectedValue(int x1, int x2, int y1, int y2, double expected)
+        {
+            // Act
+            double actual = DistanceBetweenTwoPoints.DistanceBetweenPoints(x1,x2,y1, y2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/DistanceBetweenTwoPoints.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class DistanceBetweenTwoPoints
+    {
+        public static double DistanceBetweenPoints(int x1, int y1, int x2, int y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) +
+                          Math.Pow(y2 - y1, 2));
+        }
+    }
+}
