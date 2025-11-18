30c75f3f9a6b5ceea0f1c4aa92446eec1f08e415
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-18 13:31:14 +0100

    Added valid triangle.

---
+++ Data Structures/LogicBuilding.UnitTests/ValidTriangleTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+public class ValidTriangleTests
+    {
+        [TestMethod]
+        [DataRow(3, 4, 5,true)]
+        [DataRow(7, 10, 5, true)]
+        [DataRow(1, 10, 12, false)]
+        
+        public void ValidTriangle_ByParameters_ReturnsTheExpectedValue(int a, int b, int c, bool expected)
+        {
+            // Act
+            bool actual = ValidTriangle.CheckValidity(a, b, c);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ValidTriangle_Fornegativenumber_ThrowsException()
+        {
+            // Act + Assert
+            Assert.ThrowsException<InvalidOperationException>(InvalidCall);
+        }
+        private void InvalidCall()
+        {
+            ValidTriangle.CheckValidity(-1, 4, 5);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/ValidTriangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class ValidTriangle
+    {
+        public static bool CheckValidity(int a, int b, int c)
+        {
+            if (a < 0 || b < 0 || c < 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (a + b <=  c || a + c <= b ||
+                            b + c <= a)
+                return false;
+            else
+                return true;
+        }
+    }
+}
