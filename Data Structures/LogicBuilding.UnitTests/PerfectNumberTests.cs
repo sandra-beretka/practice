82c62e95547d851d35f1949a33c2291721a7dee8
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-19 15:20:16 +0100

    Added Perfect Number.

---
+++ Data Structures/LogicBuilding.UnitTests/PerfectNumberTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class PerfectNumberTests
+    {
+        [TestMethod]
+        [DataRow(15, false)]
+        [DataRow(6, true)]
+
+        public void PerfectNumber_ByParameters_ReturnsTheExpectedValue(int n, bool expected)
+        {
+            // Act
+            bool actual = PerfectNumber.isPerfect(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/PerfectNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class PerfectNumber
+    {
+        public static bool isPerfect(int number) 
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum == number;
+        }
+    }
+}
