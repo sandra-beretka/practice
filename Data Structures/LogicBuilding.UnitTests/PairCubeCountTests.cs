79adc54a417e355b8bac4c719ed4f28854a8f038
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-19 12:29:15 +0100

    Added Pair Cube Count.

---
+++ Data Structures/LogicBuilding.UnitTests/PairCubeCountTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class PairCubeCountTests
+    {
+        [TestMethod]
+        [DataRow(9, 2)]
+        [DataRow(28, 2)]
+        [DataRow(27, 0)]
+        public void PairCubeCount_ByParameters_ReturnsTheExpectedValue(int n, int expected)
+        {
+            // Act
+            int actual = PairCubeCount.CubeCount(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/PairCubeCount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class PairCubeCount
+    {
+        public static int CubeCount(int n)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= Math.Cbrt(n); i++)
+            {
+                int cube = i * i * i;
+                int diff = n - cube;
+                if(diff==0)
+                {
+                    continue;
+                }
+
+                int cbrtDiff = (int) Math.Cbrt(diff);
+
+                if (cbrtDiff * cbrtDiff * cbrtDiff == diff)
+                { 
+                    count++; 
+                }
+                                   
+            }
+            return count;
+        }
+    }
+}
