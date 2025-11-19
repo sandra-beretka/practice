e2e51f6d77f6063e63a3e297320dad92e51a7901
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-19 15:02:05 +0100

    Added Greatest Common Divisor

---
+++ Data Structures/LogicBuilding.UnitTests/GreatestCommonDivisorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class GreatestCommonDivisorTests
+    {
+        [TestMethod]
+        [DataRow(20, 28, 4)]
+        [DataRow(60, 36, 12)]
+
+        public void GreatestCommonDivisor_ByParameters_ReturnsTheExpectedValue(int a, int b, int expected)
+        {
+            // Act
+            int actual = GreatestCommonDivisor.GCD(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/GreatestCommonDivisor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class GreatestCommonDivisor
+    {
+        public static int GCD(int a, int b) 
+        {
+            if (a == 0) return b;
+
+            if (b == 0) return a;
+
+            if (a == b) return a;
+
+            if (a > b)
+                return GCD(a - b, b);
+
+            return GCD(a, b - a);
+        }
+    }
+}
