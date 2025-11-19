a0cd7dd46dd36f9dedcf023c4ec6101279b489f9
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-19 11:59:29 +0100

    Added Factorial of a Number.

---
+++ Data Structures/LogicBuilding.UnitTests/FactorialofaNumberTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class FactorialofaNumberTests
+    {
+        [TestMethod]
+        public void FactorialofaNumber_Fornegativenumber_ThrowsException()
+        {
+            // Act + Assert
+            Assert.ThrowsException<InvalidOperationException>(InvalidCall);
+        }
+        private void InvalidCall()
+        {
+            FactorialofaNumber.Factorial(-3);
+        }
+
+        [TestMethod]
+        [DataRow(2, 2)]
+        [DataRow(3, 6)]
+        [DataRow(4, 24)]
+        public void FactorialofaNumber_ByParameters_ReturnsTheExpectedValue(int n, int expected)
+        {
+            // Act
+            int actual = FactorialofaNumber.Factorial(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/FactorialofaNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class FactorialofaNumber
+    {
+        public static int Factorial(int n)
+        {
+            int fact = 1;
+
+            if (n < 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (n == 1)
+            {
+                return fact;
+            }
+
+            for (int i = 2; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+
+            return fact;
+        }
+    }
+}
