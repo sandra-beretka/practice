94b7b3742c25c74194892948b5e268e49386a453
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-19 15:15:35 +0100

    Added Least Common Multiplier.

---
+++ Data Structures/LogicBuilding.UnitTests/LeastCommonMultiplierTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class LeastCommonMultiplierTests
+    {
+        [TestMethod]
+        [DataRow(10, 5, 10)]
+        [DataRow(11, 5, 55)]
+
+        public void LeastCommonMultiplier_ByParameters_ReturnsTheExpectedValue(int a, int b, int expected)
+        {
+            // Act
+            int actual = LeastCommonMultiplier.LCM(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/LeastCommonMultiplier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class LeastCommonMultiplier
+    {
+        public static int LCM(int a, int b) 
+        {
+            int x = 0;
+            if (a == 0 || b == 0)
+            {
+                return x;
+            }
+            else 
+            {
+                int y = GreatestCommonDivisor.GCD(a, b);
+                x = (a * b) / y;
+                return x;
+            }                       
+        }
+    }
+}
