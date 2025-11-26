87fac6686b165c829117825d95369e901062a589
Sandra Beretka <sandra.beretka@gmail.com>
2025-11-26 11:47:57 +0100

    Added Fibonacci.

---
+++ Data Structures/LogicBuilding.UnitTests/FibonacciTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        [DataRow(2, 1)]
+        [DataRow(5, 5)]
+
+        public void NthFibonacci_ByParameters_ReturnsTheExpectedValue(int n, int expected)
+        {
+            // Act
+            int actual = Fibonacci.NthFibonacciNumber(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class Fibonacci
+    {
+        public static int NthFibonacciNumber(int n) 
+        {
+            
+            if (n <= 1)
+            {
+                return n;
+            }
+            else 
+            {
+                return NthFibonacciNumber(n - 1) + NthFibonacciNumber(n - 2);
+            }  
+            
+        }
+    }
+}
