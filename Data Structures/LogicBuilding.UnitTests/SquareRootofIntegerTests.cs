892a434228e4384b7cabb910cc4833618a3b2679
Sandra Beretka <sandra.beretka@gmail.com>
2025-12-03 14:45:15 +0100

    Added Square Root of an Integer.

---
+++ Data Structures/LogicBuilding.UnitTests/SquareRootofIntegerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class SquareRootofIntegerTests
+    {
+        [TestMethod]
+        [DataRow(11, 3)]
+        [DataRow(4, 2)]
+
+        public void SquareRoot_ByParameters_ReturnsTheExpectedValue(int n, int expected)
+        {
+            // Act
+            int actual = SquareRootofInteger.FloorSquareRoot(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/SquareRootofInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class SquareRootofInteger
+    {
+        public static int FloorSquareRoot(int n)
+        {
+            int lo = 1;
+            int hi = n;
+            int res = 1;
+
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+
+                if (mid * mid <= n)
+                {
+                    res = mid;
+                    lo = mid + 1;
+                }
+
+                else
+                {
+                    hi = mid - 1;
+                }
+            }
+            return res;
+        }
+    }
+}
