bcf72e5a448caa0e08eebaf8245441c858902908
Sandra Beretka <sandra.beretka@gmail.com>
2025-12-03 15:34:23 +0100

    Added Divisibleby4.

---
+++ Data Structures/LogicBuilding.UnitTests/Divisibleby4Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class Divisibleby4Tests
+    {
+        [TestMethod]
+        [DataRow(1124, true)]
+        [DataRow(4, true)]
+        [DataRow(5, false)]
+        [DataRow(589333862, false)]
+        
+        public void Divisibleby4_ByParameters_ReturnsTheExpectedValue(int n, bool expected)
+        {
+            // Act
+            bool actual = Divisibleby4.nDivisibleby4(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/Divisibleby4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class Divisibleby4
+    {
+        public static bool twodigitnumberdivisibleby4(int n)
+        {
+            if (n % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool nDivisibleby4 (int n)
+        {
+            int tdn = 0;
+            if (n > -99 || n < 99)
+            {
+                return twodigitnumberdivisibleby4(n);
+            }
+            else 
+            {
+                for (int i = 1; i <= 2; i++) 
+                {
+                    tdn = tdn * 10 + n % 10;
+                    n /= 10;
+                }
+                return twodigitnumberdivisibleby4(tdn);
+            }
+        }
+    }
+}
