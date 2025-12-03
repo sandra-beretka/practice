950bcaf14ac4937967cdc3964a29e9b2429d4bcc
Sandra Beretka <sandra.beretka@gmail.com>
2025-12-03 15:20:07 +0100

    Added Exactly 3 Divisors.

---
+++ Data Structures/LogicBuilding.UnitTests/Exactly3DivisorsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class Exactly3DivisorsTests
+    {
+        [TestMethod]
+        [DataRow(16, 2)]
+        [DataRow(100, 4)]
+
+        public void Exactly3Divisors_ByParameters_ReturnsTheExpectedValue(int n, int expected)
+        {
+            // Act
+            int actual = Exactly3Divisors.CountExactlz3Divisors(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/Exactly3Divisors.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class Exactly3Divisors
+    {
+        public static int CountExactlz3Divisors(int n) 
+        {
+            int counter = 0;
+
+            for (int i = 2; i <= n; i++) 
+            {
+                if (PrimeNumbers.IsPrime(i)) 
+                {
+                    if (i * i <= n) 
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+    }
+}
