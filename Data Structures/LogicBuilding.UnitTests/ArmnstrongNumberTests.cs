3e12dbe2474e2234574c2cc2304e40e7a122bfb5
Sandra Beretka <sandra.beretka@gmail.com>
2025-12-03 12:57:01 +0100

    Added Armnstrong Number Checker.

---
+++ Data Structures/LogicBuilding.UnitTests/ArmnstrongNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding.UnitTests
+{
+    [TestClass]
+    public class ArmnstrongNumberTests
+    {
+        [TestMethod]
+        [DataRow(153,true)]
+        [DataRow(9474, true)]
+        [DataRow(123, false)]
+
+        public void ArmnstrongNumber_ByParameters_ReturnsTheExpectedValue(int n, bool expected)
+        {
+            // Act
+            bool actual = ArmnstrongNumber.CheckArmnstrongNumber(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(-2)]
+        public void ArmnstrongNumber_For0_ThrowsException(int n)
+        {
+            // Act + Assert
+            Assert.ThrowsException<InvalidOperationException>(() => ArmnstrongNumber.CheckArmnstrongNumber(n));
+        }
+    }
+}
---
+++ Data Structures/LogicBuilding/ArmnstrongNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicBuilding
+{
+    public class ArmnstrongNumber
+    {
+        public static int Lengtofn(int n)
+        {
+            int t = 0;
+            while (n != 0)
+            {
+                t++;
+                n /= 10;
+            }
+            return t;
+        }
+        public static int CreateArmnstrongNumber(int n)
+        {
+            int an = 0;
+            int rem = 0;
+            int t=Lengtofn(n);
+            while (n != 0)
+            {
+                rem = n % 10;
+                an = an + (int)Math.Pow(rem, t);
+                n /= 10;
+            }
+            return an;
+        }
+        public static bool CheckArmnstrongNumber(int n)
+        {
+            if (n <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+           int an=CreateArmnstrongNumber(n);
+
+            //CheckThenumbern
+            return an == n;
+        }
+    }
+}
